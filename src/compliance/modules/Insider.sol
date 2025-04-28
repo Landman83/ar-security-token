c1c7576b3d33ef06395485235b3a15c3ffd362c8
Rankin Poage <rpoage@proton.me>
2025-04-28 11:07:45 -0500

    committing before implementing insider registry

---
+++ src/compliance/modules/Insider.sol
@@ -0,0 +1,1 @@
+// will eventually add insider-specific restrictions here
---
+++ test/Insider.t.sol
@@ -0,0 +1,3 @@
+// tests insider functionality
+// should test mints, transfers, and transfers from, to, and within the whitelist 
+// should test that transfers outside the whitelist who are also unaccredited are restricted
